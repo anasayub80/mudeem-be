291ee1b97618a19685faba0c1a80efc44d5dbb75
Ahmed Hassan <ah2k.dev@gmail.com>
2024-12-21 01:09:45 +0500

    Update collab-forum.controller.ts

--- src/controllers/collab-forum/collab-forum.controller.ts
+++ src/controllers/collab-forum/collab-forum.controller.ts
@@ -1,14 +1,122 @@
 import { RequestHandler } from 'express';
+import ErrorHandler from 'utils/errorHandler';
 
-const createPost: RequestHandler = async (req, res) => {};
-const getAllPosts: RequestHandler = async (req, res) => {};
-const getPost: RequestHandler = async (req, res) => {};
-const updatePost: RequestHandler = async (req, res) => {};
-const likeUnlikePost: RequestHandler = async (req, res) => {};
-const createComment: RequestHandler = async (req, res) => {};
-const deleteComment: RequestHandler = async (req, res) => {};
-const likeUnlikeComment: RequestHandler = async (req, res) => {};
-const createReply: RequestHandler = async (req, res) => {};
+const createPost: RequestHandler = async (req, res) => {
+  // #swagger.tags = ['collab-forum']
+  try {
+  } catch (error) {
+    return ErrorHandler({
+      message: (error as Error).message,
+      statusCode: 500,
+      req,
+      res
+    });
+  }
+};
+const getAllPosts: RequestHandler = async (req, res) => {
+  // #swagger.tags = ['collab-forum']
+
+  try {
+  } catch (error) {
+    return ErrorHandler({
+      message: (error as Error).message,
+      statusCode: 500,
+      req,
+      res
+    });
+  }
+};
+const getPost: RequestHandler = async (req, res) => {
+  // #swagger.tags = ['collab-forum']
+
+  try {
+  } catch (error) {
+    return ErrorHandler({
+      message: (error as Error).message,
+      statusCode: 500,
+      req,
+      res
+    });
+  }
+};
+const updatePost: RequestHandler = async (req, res) => {
+  // #swagger.tags = ['collab-forum']
+
+  try {
+  } catch (error) {
+    return ErrorHandler({
+      message: (error as Error).message,
+      statusCode: 500,
+      req,
+      res
+    });
+  }
+};
+const likeUnlikePost: RequestHandler = async (req, res) => {
+  // #swagger.tags = ['collab-forum']
+
+  try {
+  } catch (error) {
+    return ErrorHandler({
+      message: (error as Error).message,
+      statusCode: 500,
+      req,
+      res
+    });
+  }
+};
+const createComment: RequestHandler = async (req, res) => {
+  // #swagger.tags = ['collab-forum']
+
+  try {
+  } catch (error) {
+    return ErrorHandler({
+      message: (error as Error).message,
+      statusCode: 500,
+      req,
+      res
+    });
+  }
+};
+const deleteComment: RequestHandler = async (req, res) => {
+  // #swagger.tags = ['collab-forum']
+
+  try {
+  } catch (error) {
+    return ErrorHandler({
+      message: (error as Error).message,
+      statusCode: 500,
+      req,
+      res
+    });
+  }
+};
+const likeUnlikeComment: RequestHandler = async (req, res) => {
+  // #swagger.tags = ['collab-forum']
+
+  try {
+  } catch (error) {
+    return ErrorHandler({
+      message: (error as Error).message,
+      statusCode: 500,
+      req,
+      res
+    });
+  }
+};
+const createReply: RequestHandler = async (req, res) => {
+  // #swagger.tags = ['collab-forum']
+
+  try {
+  } catch (error) {
+    return ErrorHandler({
+      message: (error as Error).message,
+      statusCode: 500,
+      req,
+      res
+    });
+  }
+};
 
 export {
   createPost,
